21070cf8c02a6e64798f54f9793793daa8180d63
Tatiana_Pakulnitskaya <takovenya@gmail.com>
2023-11-19 01:11:59 +0300

    Initial commit

---
+++ src/test/java/MyTestRunner/TestRunner.java
@@ -0,0 +1,11 @@
+package MyTestRunner;
+
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(features = "Features", glue = "stepDefinitions")
+public class TestRunner {
+}
